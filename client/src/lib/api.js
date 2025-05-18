7be0ba42ecdb0cede778d6733990e8a9f3b6553c
mohammadalnseirat <nseiratma@gmail.com>
2025-05-18 19:11:26 +0400

    Final Update

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -14,66 +14,66 @@ export const login = async (userData) => {
 
 //! Function to logout:
 export const logout = async () => {
-  const res = await axiosInstance.post("/auth/log-out");
+  const res = await axiosInstance.post("/v1/auth/log-out");
   return res.data;
 };
 
 //! Function to get auth user:
 export const getAuthUser = async () => {
-  const res = await axiosInstance.get("/auth/me");
+  const res = await axiosInstance.get("/v1/auth/me");
   return res.data;
 };
 
 //! Function to complete onboarding:
 export const completeOnboarding = async (userData) => {
-  const res = await axiosInstance.post("/auth/on-boarding", userData);
+  const res = await axiosInstance.post("/v1/auth/on-boarding", userData);
   return res.data;
 };
 
 //! Function to get the user friends:
 export const getUserFriends = async () => {
-  const res = await axiosInstance.get("/users/friends");
+  const res = await axiosInstance.get("/v1/users/friends");
   return res.data;
 };
 
 //! Function to get the recommended users:
 export const getRecommendedUsers = async () => {
-  const res = await axiosInstance.get("/users");
+  const res = await axiosInstance.get("/v1/users");
   return res.data;
 };
 
 //! Function to get the outgoing requests:
 export const getOutgoingRequests = async () => {
-  const res = await axiosInstance.get("/users/outgoing-friend-requests");
+  const res = await axiosInstance.get("/v1/users/outgoing-friend-requests");
   return res.data;
 };
 
 //! Function to send a friend request:
 export const sendFriendRequest = async (userId) => {
-  const res = await axiosInstance.post(`/users/friend-request/${userId}`);
+  const res = await axiosInstance.post(`/v1/users/friend-request/${userId}`);
   return res.data;
 };
 
 //! Function to get the friend requests:
 export const getFriendRequests = async () => {
-  const res = await axiosInstance.get("/users/friend-requests");
+  const res = await axiosInstance.get("/v1/users/friend-requests");
   return res.data;
 };
 
 //! Function to accept a friend request:
 export const acceptFriendRequest = async (userId) => {
-  const res = await axiosInstance.put(`/users/friend-request/${userId}/accept`);
+  const res = await axiosInstance.put(`/v1/users/friend-request/${userId}/accept`);
   return res.data;
 };
 
 //! Function to reject a friend request:
 export const rejectFriendRequest = async (userId) => {
-  const res = await axiosInstance.put(`/users/friend-request/${userId}/reject`);
+  const res = await axiosInstance.put(`/v1/users/friend-request/${userId}/reject`);
   return res.data;
 };
 
 // ! Function to get the stream token:
 export const getStreamToken = async () => {
-  const res = await axiosInstance.get("/chat/token");
+  const res = await axiosInstance.get("/v1/chat/token");
   return res.data;
 };
--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const BASE_URL =
   import.meta.env.MODE === "development"
-    ? `${import.meta.env.VITE_BACK_END_URL}/api/v1`
-    : "/api/v1";
+    ? `${import.meta.env.VITE_BACK_END_URL}/api`
+    : "/api";
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
